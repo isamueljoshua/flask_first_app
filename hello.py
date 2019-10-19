b39dd23874f0768d9ce8abe575fc5800f93be9aa
Samuel Joshua <isamueljoshua@gmail.com>
2019-10-19 23:23:31 +0530

    passing parameters in urls

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, escape
 app = Flask(__name__)
 
 # This route is to go to the index page
@@ -10,3 +10,21 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+# Varibable names in URL to make it dynamic
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+# use a converter to specify the type of the argument
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+# path - like string but also accepts slashes
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
