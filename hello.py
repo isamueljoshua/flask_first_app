1ae2f970cb1460b3acc66dcaefdeefd9948628e2
Samuel Joshua <isamueljoshua@gmail.com>
2019-10-19 22:02:46 +0530

    added meaningful urls

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+# This route is to go to the index page
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page'
+
+# This route is to go to hello page
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
