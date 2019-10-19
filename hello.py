0748f1f40ed690e7f2b704ee4970fc74f10bad4e
Samuel Joshua <isamueljoshua@gmail.com>
2019-10-19 23:30:10 +0530

    unique urls and their redirection behaviour

--- hello.py
+++ hello.py
@@ -28,3 +28,15 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % escape(subpath)
+
+# It’s similar to a folder in a file system. 
+# If you access the URL without a trailing slash, Flask redirects you to the canonical URL with the trailing slash.
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+# It’s similar to the pathname of a file. Accessing the URL with a trailing slash produces a 404 “Not Found” error. 
+# This helps keep URLs unique for these resources, which helps search engines avoid indexing the same page twice.
+@app.route('/about')
+def about():
+    return 'The about page'
